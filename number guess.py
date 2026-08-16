5e9cf0afc8f65823b73134612bbac57229e2a03b
Civan Nur <civannurk@gmail.com>
2026-08-16 14:07:49 +0300

    added a lives system

--- number guess.py
+++ number guess.py
@@ -1,12 +1,21 @@
 #random'u çağıralım
 import random
-bilgisayarın_seçimi=random.randint(1,20)
-kullanıcının_tahmini=0
-while kullanıcının_tahmini!=bilgisayarın_seçimi:
-    kullanıcının_tahmini=int(input("1 ile 20 arasında bir sayı tahmin edin: "))
-    if kullanıcının_tahmini<bilgisayarın_seçimi:
-         print("daha büyük bir sayı seç.")
-    elif kullanıcının_tahmini>bilgisayarın_seçimi:
+bilgisayarin_secimi=random.randint(1,20)
+hak=3
+kullanicinin_tahmini=0
+
+while kullanicinin_tahmini!=bilgisayarin_secimi and hak>0:
+    kullanicinin_tahmini=int(input("1-20 arasında bir sayı tahmin ediniz: "))
+    
+    if kullanicinin_tahmini<bilgisayarin_secimi:
+        print("daha büyük bir sayı seç.")
+        hak-=1
+        print("kalan hakkınız:",hak)
+    elif kullanicinin_tahmini>bilgisayarin_secimi:
         print("daha küçük bir sayı seç.")
+        hak-=1
+        print("kalan hakkınız:",hak)
+        
     else:
         print("helal lan! doğru bildin")
+print("oyun bitti. bilgisayarın seçtiği sayı,",bilgisayarin_secimi)
