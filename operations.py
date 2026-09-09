b6bc7ecfe7cb5fddce66e34041e355c37e43bf21
Civan Nur <civannurk@gmail.com>
2026-09-09 14:33:12 +0300

    calculator operatins module

---
+++ operations.py
@@ -0,0 +1,13 @@
+
+#işlem fonksiyonumz
+def hesaplama(a, b, işlem):
+    a=float(a)
+    b=float(b)
+    if işlem=="+":
+        return a+b 
+    elif işlem=="-":
+       return a-b
+    elif işlem=="x":
+        return a*b
+    elif işlem=="÷":
+        return a/b
